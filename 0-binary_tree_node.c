7155572f52f2804903792ff9dc4507ecd2afa1b4
ronniebm <ronnie.coding@gmail.com>
2020-07-20 12:00:59 -0500

    first commit task#0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node.
+ * ----------------------------------------------
+ * @parent: a pointer to the parent node of the node to create.
+ * @value: the value to put in the new node.
+ * ---------------------------------------------
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	new_node = calloc(sizeof(binary_tree_t), 1);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
