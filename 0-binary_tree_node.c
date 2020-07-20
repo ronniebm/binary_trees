8020c74b218c97daeefe119e3baab2d442b7f127
ronniebm <ronnie.coding@gmail.com>
2020-07-20 13:31:32 -0500

    fixing return NULL if failure

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,6 +5,8 @@
  * ----------------------------------------------
  * @parent: a pointer to the parent node of the node to create.
  * @value: the value to put in the new node.
+ *
+ * Return: a pointer to the new node, or NULL on failure.
  * ---------------------------------------------
  */
 
@@ -13,6 +15,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new_node = NULL;
 
 	new_node = calloc(sizeof(binary_tree_t), 1);
+
+	if (!new_node)
+		return (NULL);
+
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
