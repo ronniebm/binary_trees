881da8e964b7a20948a42552144035f93cdbec3f
ronniebm <ronnie.coding@gmail.com>
2020-07-21 20:42:20 -0500

    first commit task#18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node.
+ * -------------------------------------------------
+ * @node: a pointer to the node to find the uncle.
+ *
+ * Return: a pointer to uncle node, otherwise: NULL.
+ * -------------------------------------------------
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+
+	binary_tree_t *a;
+	binary_tree_t *b;
+
+	if (!node || !node->parent->parent)
+		return (NULL);
+
+	else
+	{
+		a = node->parent->parent->left;
+		b = node->parent->parent->right;
+	}
+
+	if ((a && a->left && (node->n != a->left->n)) &&
+		(a && a->right && (node->n != a->right->n)))
+		return (a);
+
+	else if ((b && b->left && (node->n != b->left->n)) &&
+			 (b && b->right && (node->n != b->right->n)))
+		return (b);
+
+	else
+		return (NULL);
+
+}
