fae1912a40c14a8e2764ff7682ac198459ff697e
ronniebm <ronnie.coding@gmail.com>
2020-07-22 10:33:56 -0500

    fixing task#13 try1

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,34 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * children_of - counts the nodes with at least 1
- * child in a binary tree.
- * ----------------------------------------------
- * @node: a pointer to a given node.
- *
- * Return: Number of nodes with at least 1 child.
- * ----------------------------------------------
- */
-
-int children_of(const binary_tree_t *node)
-{
-	if (node == NULL)
-		return (0);
-
-	if ((!node->left && node->right) ||
-		(node->left && !node->right))
-		return (1);
-
-	else
-	{
-		if (!node->parent)
-			return (children_of(node->left) + children_of(node->right) + 1);
-	}
-
-	return (children_of(node->left) + children_of(node->right));
-}
-
-/**
  * binary_tree_nodes - counts the nodes with at least 1
  * child in a binary tree.
  * ----------------------------------------------------
@@ -40,11 +12,14 @@ int children_of(const binary_tree_t *node)
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	int size = 0;
 
 	if (!tree)
 		return (0);
 
-	size = children_of(tree);
-	return (size);
+	if (tree->left || tree->right)
+		return (binary_tree_nodes(tree->left) +
+				binary_tree_nodes(tree->right) + 1);
+
+	return (0);
+
 }
