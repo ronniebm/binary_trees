b799c5d08a9cbd08a0a87eb9fc8c73c668543b1f
ronniebm <ronnie.coding@gmail.com>
2020-07-21 21:13:25 -0500

    fixing task#0 try2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
 
-	new_node = calloc(sizeof(binary_tree_t), 1);
+	new_node = malloc(sizeof(binary_tree_t));
 
 	if (!parent || !new_node)
 		return (NULL);
